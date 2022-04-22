855a0bc6159a9a7c514f215a13f5ad16841f3727
Alexander Aymond <lynxkhaos@gmail.com>
2022-04-22 17:45:26 -0500

    Complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import Character from './components/Character';
+import styled from 'styled-components';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -7,11 +10,34 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+const [data, setData] = useState([])
 
+useEffect(() => {
+  axios.get('https://swapi.dev/api/people/')
+  .then(res => {
+    setData(res.data)
+  }).catch(err => console.error(err))
+}, [])
+
+
+const StyledApp = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+height: 100vh;
+`
+const StyledDiv = styled.div`
+width: 90%;
+`
   return (
-    <div className="App">
+    <StyledApp className="App">
       <h1 className="Header">Characters</h1>
-    </div>
+      <StyledDiv>
+      { data.length ? data.map(data => {
+        return <Character data={data} />
+      }) : <p>Page is Loading</p>}
+      </StyledDiv>
+    </StyledApp>
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,40 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+ const Character = (props) => {
+    const StyledDiv = styled.div`
+    background-image: url('https://imgs.search.brave.com/lh2K6y0zTLoY_Ta21ZIDyZ4XVEVlF7aQdnYtNAojVYM/rs:fit:736:736:1/g:ce/aHR0cHM6Ly9zLW1l/ZGlhLWNhY2hlLWFr/MC5waW5pbWcuY29t/LzczNngvNWEvYzEv/OWEvNWFjMTlhN2Yz/YmNiNjhjOTcyZGYy/MmE5MzRlMzAyOTMu/anBn');
+    border: solid 2px lemonchiffon;
+    width: 50%
+    margin: 1em;
+    margin-left: 13em;
+    margin-right: 14em;
+    margin-top: 1em;
+    `
+    const StyledH3 = styled.h3`
+    font-size: 2rem;
+    padding-top: .2em;
+    color: yellow;
+    text-shadow: 3px 3px black;
+    `
+
+    const StyledP = styled.p`
+    font-size: 1rem;
+    color: white;
+    text-shadow: 1px 1px black;
+    `
+    
+    return (
+        <StyledDiv>
+            <StyledH3>Name: {props.data.name}</StyledH3>
+            <StyledP>Birth Year: {props.data.birth_year}</StyledP>
+            <StyledP>Eye Color: {props.data.eye_color}</StyledP>
+            <StyledP>Gender: {props.data.gender}</StyledP>
+            <StyledP>Hair Color: {props.data.hair_color}</StyledP>
+            <StyledP>Height: {props.data.height}</StyledP>
+            <StyledP>Skin Color: {props.data.skin_color}</StyledP>
+        </StyledDiv>
+    )
+}
+export default Character
